c43c9b2e825701b57ac2a14fd0a89b1cf8ccdaa4
Daniel Antunes <daniel.antunes1697@gmail.com>
2018-04-18 06:48:27 -0300

    add index and html service provider

---
+++ app/Providers/HtmlServiceProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Html\FormBuilder;
+use Illuminate\Html\HtmlBuilder;
+use Illuminate\Html\HtmlServiceProvider as ServiceProvider;
+
+class HtmlServiceProvider extends ServiceProvider
+{
+    /**
+     * Register the HTML builder instance.
+     *
+     * @return void
+     */
+    protected function registerHtmlBuilder()
+    {
+        $this->app->singleton('html', function ($app) {
+            return new HtmlBuilder($app['url']);
+        });
+    }
+
+    /**
+     * Register the form builder instance.
+     *
+     * @return void
+     */
+    protected function registerFormBuilder()
+    {
+        $this->app->singleton('form', function ($app) {
+            $form = new FormBuilder($app['html'], $app['url'], $app['session.store']->getToken());
+
+            return $form->setSessionStore($app['session.store']);
+        });
+    }
+}
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
         <title>Loja Virtual</title>
-        {!! HTML::script('js/jquery-3.3.1.min.js') !!}
+        {!! Html::script('js/jquery-3.3.1.min.js') !!}
     </head>
     <body>
     <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center"></div>
