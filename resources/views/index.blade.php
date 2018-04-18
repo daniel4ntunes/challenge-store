9c1d4d066823e1c953decf0bf4de27a668229176
Daniel Antunes <daniel.antunes1697@gmail.com>
2018-04-18 09:23:11 -0300

    layout

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,17 +1,73 @@
-@extends('main') 
-@section('content')
+@extends('main') @section('content')
+<div class="">
     <div class="text-center">
-        <h1 class="display-5">Loja Virtual</h1>
-        <div class="text-center">
-            <a type="button" class="btn btn-outline-primary" onclick="">Start</a>
-        </div>
-        <footer class="pt-4 my-md-5 pt-md-5 border-top">
-            <div class="col-6 col-md text-center">
-                <ul class="list-unstyled text-small">
-                    <!-- <li><a class="text-muted" target="_blank" href="https://docs.google.com/document/d/1VZuUwwern7a7FbgG4DG7-LscHMpHwMYUE_zrS118U7s/edit">Challenge</a></li> -->
-                    <!-- <li><a class="text-muted" target="_blank" href="https://github.com/hermeto/gclub">Source</a></li> -->
-                </ul>
-            </div>
-        </footer>
+        <h1 class="mt-5">Loja Virtual</h1><hr>
+        <!-- <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus venenatis diam id scelerisque eleifend. Aenean volutpat
+            ut metus non blandit. Mauris felis tortor, accumsan non urna quis, auctor semper lorem. Ut gravida feugiat tortor,
+            id laoreet massa feugiat vel. Vestibulum dapibus ultrices semper. Donec porta laoreet aliquam. Sed at placerat
+            sapien. Nulla facilisi. Fusce accumsan odio at massa sodales eleifend. Proin rhoncus in sem a tincidunt. Praesent
+            mi quam, aliquet accumsan cursus ut, ultricies ullamcorper quam. Pellentesque dapibus nibh pharetra enim imperdiet
+            tincidunt. Integer in luctus nisi, eget finibus magna. Duis tincidunt placerat fermentum. Vivamus accumsan tincidunt
+            faucibus. In erat purus, congue et lorem non, sollicitudin consequat magna. Sed facilisis odio justo. Nulla facilisi.
+            Phasellus auctor ligula et dictum bibendum. Nunc eleifend quis metus a ultrices. Sed mattis arcu at finibus mattis.
+            Curabitur eget felis nec metus rutrum semper. Donec eget vehicula mi. Quisque finibus libero eget libero porta,
+            nec scelerisque tellus fermentum. Sed at sodales augue. Quisque fermentum sollicitudin enim, sit amet lobortis
+            eros porttitor ac. Etiam euismod est sed enim faucibus, at lacinia nisl luctus. Ut condimentum eget massa maximus
+            cursus. Etiam pretium, sapien nec eleifend consequat, eros massa pharetra sapien, congue dignissim tellus nisl
+            vel nibh.
+        </p> -->
+        <!-- <button class="btn btn-outline-primary my-2 my-sm-0" onclick="">Start</button> -->
     </div>
+    <div class="card-deck">
+        <div class="card" style="width: 18rem;">
+            <img src="img/doom.jpg" alt="Card image cap" class="card-img-top img-fluid">
+            <!-- {!! Html::image('img/assassinscreed.jpg', 'Card image cap"', array('class' => 'card-img-top', 'style' => 'width')) !!} -->
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+            <div class="card-footer">
+                <button type="button" class="btn btn-primary pull-right">Comprar</button>
+                <button type="button" class="btn btn-secondary pull-left">Detalhes</button>
+            </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+            <img src="img/fallout.jpg" alt="Card image cap" class="card-img-top img-fluid">
+            <!-- {!! Html::image('img/watchdogs.jpg', 'Card image cap"', array('class' => 'card-img-top')) !!} -->
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+            <div class="card-footer">
+                <button type="button" class="btn btn-primary pull-right">Comprar</button>
+                <button type="button" class="btn btn-secondary pull-left">Detalhes</button>
+            </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+            <img src="img/ufc.jpg" alt="Card image cap" class="card-img-top img-fluid">
+            <!-- {!! Html::image('img/watchdogs.jpg', 'Card image cap"', array('class' => 'card-img-top')) !!} -->
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+            <div class="card-footer">
+                <button type="button" class="btn btn-primary pull-right">Comprar</button>
+                <button type="button" class="btn btn-secondary pull-left">Detalhes</button>
+            </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+            <img src="img/zombi.png_large" alt="Card image cap" class="card-img-top img-fluid">
+            <!-- {!! Html::image('img/watchdogs.jpg', 'Card image cap"', array('class' => 'card-img-top')) !!} -->
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+            <div class="card-footer">
+                <button type="button" class="btn btn-primary pull-right">Comprar</button>
+                <button type="button" class="btn btn-secondary pull-left">Detalhes</button>
+            </div>
+        </div>
+    </div>
+</div>
 @stop
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,18 +1,57 @@
-
-<!doctype html> 
+<!doctype html>
 <html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-        <title>Loja Virtual</title>
-        {!! Html::script('js/jquery-3.3.1.min.js') !!}
-    </head>
-    <body>
-    <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center"></div>
-    <div class="container">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="icon" href="img/logo_gamersXYZ.png" type="image/png" sizes="16x16">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+        crossorigin="anonymous">
+    <title>GamersXYZ</title>
+    {!! Html::script('js/jquery-3.3.1.min.js') !!}
+</head>
+
+<body>
+    <header>
+        <!-- Fixed navbar -->
+        <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+            <a class="navbar-brand" href="/">
+                <img src="img/logo_gamersXYZ.png" width="30" height="30" class="d-inline-block align-top" alt=""> GamersXYZ
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse"
+                aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarCollapse">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="/">Home
+                            <span class="sr-only">(current)</span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Carrinho
+                            <span class="badge badge-pill badge-light">0</span>
+                        </a>
+                    </li>
+                </ul>
+                <form class="form-inline mt-2 mt-md-0">
+                    <input class="form-control mr-sm-2" type="text" aria-label="Search">
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
+                </form>
+            </div>
+        </nav>
+    </header>
+
+    <div class="container" style="margin-top: 100px;">
         @yield('content')
     </div>
-    <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center"></div>
-    </body>
+
+    <!-- <footer class="footer">
+        <div class="container">
+            <span class="text-muted">Place sticky footer content here.</span>
+        </div>
+    </footer> -->
+</body>
+
 </html>
