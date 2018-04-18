5e3bc808f6e59300cb5cc7bf0ba095f972054f10
Daniel Antunes <daniel.antunes1697@gmail.com>
2018-04-18 06:29:15 -0300

    composer

---
+++ resources/views/index.blade.php
@@ -0,0 +1,17 @@
+@extends('main') 
+@section('content')
+    <div class="text-center">
+        <h1 class="display-5">Loja Virtual</h1>
+        <div class="text-center">
+            <a type="button" class="btn btn-outline-primary" onclick="">Start</a>
+        </div>
+        <footer class="pt-4 my-md-5 pt-md-5 border-top">
+            <div class="col-6 col-md text-center">
+                <ul class="list-unstyled text-small">
+                    <!-- <li><a class="text-muted" target="_blank" href="https://docs.google.com/document/d/1VZuUwwern7a7FbgG4DG7-LscHMpHwMYUE_zrS118U7s/edit">Challenge</a></li> -->
+                    <!-- <li><a class="text-muted" target="_blank" href="https://github.com/hermeto/gclub">Source</a></li> -->
+                </ul>
+            </div>
+        </footer>
+    </div>
+@stop
---
+++ resources/views/main.blade.php
@@ -0,0 +1,18 @@
+
+<!doctype html> 
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <title>Loja Virtual</title>
+        {!! HTML::script('js/jquery-3.3.1.min.js') !!}
+    </head>
+    <body>
+    <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center"></div>
+    <div class="container">
+        @yield('content')
+    </div>
+    <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center"></div>
+    </body>
+</html>
