92b99e9fd16e41909d582ae3a44e21dfeac91dea
Daniel Antunes <daniel.antunes1697@gmail.com>
2018-04-21 18:29:54 -0300

    modified accountController

--- app/Http/Controllers/Account/AccountController.php
+++ app/Http/Controllers/Account/AccountController.php
@@ -6,10 +6,10 @@ use App\Http\Controllers\Controller;
 
 class AccountController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
+    // public function __construct()
+    // {
+    //     $this->middleware('auth');
+    // }
 
     public function indexAction()
     {
